b8529138763f73cb63a78e6a501b3fc93650b973
Praveen Dhankhar <praveen-dhankhar@users.noreply.github.com>
2026-03-15 15:31:25 +0530

    Add api/src/types/to.ts

---
+++ api/src/types/to.ts
@@ -0,0 +1,49 @@
+import { CANCEL_ORDER, CREATE_ORDER, GET_BALANCE, GET_DEPTH, GET_OPEN_ORDERS, ON_RAMP, WITHDRAW } from "."
+
+export type MessageToEngine = {
+    type: typeof CREATE_ORDER,
+    data: {
+        market: string,
+        price: string,
+        quantity: string,
+        side: "buy" | "sell",
+        userId: string
+    }
+} | {
+    type: typeof CANCEL_ORDER,
+    data: {
+        orderId: string,
+        market: string,
+    }
+} | {
+    type: typeof ON_RAMP,
+    data: {
+        amount: string,
+        userId: string,
+        txnId: string
+    }
+} | {
+    type: typeof WITHDRAW,
+    data: {
+        amount: string,
+        userId: string,
+        bankAccount: string,
+        txnId: string
+    }
+} | {
+    type: typeof GET_DEPTH,
+    data: {
+        market: string,
+    }
+} | {
+    type: typeof GET_OPEN_ORDERS,
+    data: {
+        userId: string,
+        market: string,
+    }
+} | {
+    type: typeof GET_BALANCE,
+    data: {
+        userId: string,
+    }
+}
